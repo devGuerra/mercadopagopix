ff71f82a550b0a27dc4ee2258460f37c3792085b
Roberto Guerra <contato@robertoguerra.dev>
2025-02-16 19:13:03 +0000

    chore: log data payment

--- app/api/webhooks/mercadopago/route.ts
+++ app/api/webhooks/mercadopago/route.ts
@@ -1,5 +1,8 @@
 import { validateMercadoPago } from "@/app/middlewares/validateMercadoPago";
+import MercadoPagoConfig, { Payment } from "mercadopago";
 import { NextResponse, type NextRequest } from "next/server";
+
+const MERCADOPAGO_SECRET = process.env.MP_SECRET as string;
 
 export async function POST(req: NextRequest) {
   const validationError = await validateMercadoPago(req);
@@ -8,6 +11,20 @@ export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
     console.log("Notificação recebida:", body);
+
+    if (body.action === "payment.created") {
+    }
+
+    if (body.action === "payment.updated") {
+      const client = new MercadoPagoConfig({
+        accessToken: MERCADOPAGO_SECRET,
+      });
+      const payment = new Payment(client);
+
+      const paymentData = payment.get({ id: body.data.id });
+
+      console.log(paymentData);
+    }
 
     return NextResponse.json({ success: true }, { status: 200 });
   } catch (error) {
